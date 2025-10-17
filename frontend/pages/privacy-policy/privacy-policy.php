c35587bb74620a638d163281861fd3307fe90453
maevee33 <hannah.zepeda03@gmail.com>
2025-10-17 13:25:39 +0800

    about

--- frontend/pages/privacy-policy/privacy-policy.php
+++ frontend/pages/privacy-policy/privacy-policy.php
@@ -67,9 +67,6 @@ $conn->close();
     <title><?php echo htmlspecialchars($privacy['title']); ?> - Neo Exclusive Cafe</title>
     <link rel="stylesheet" href="privacy-policy.css">
     <link rel="icon" type="image/x-icon" href="../../../assets/images/favicon.ico">
-    
-    <!-- Include user navigation -->
-    <?php include "../../user-includes/user-header.php"; ?>
 </head>
 <body>
     <!-- Navigation -->
